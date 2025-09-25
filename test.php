07dc47b496187360b6ddf620637db0ecbf9f9c84
Radel Magundayao <radelmagundayao0@gmail.com>
2025-09-25 09:31:06 +0800

    First Commit

--- test.php
+++ test.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "sql12.freesqldatabase.com";  // Database Host
-$username   = "sql12798325";                // Database Username
+$username   = "sql12800008";                // Database Username
 $password   = "2nKjASgD3l";                  // Database Password
-$database   = "sql12798325";                // Database Name
+$database   = "sql12800008";                // Database Name
 $port       = 3306;                         // Default MySQL port
 
 $conn = mysqli_connect($servername, $username, $password, $database, $port);
