2150d33631f6e32937990b5875e27e13f2d981f5
Radel Magundayao <radelmagundayao0@gmail.com>
2025-10-09 11:05:34 +0800

    First Commit

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> getenv('DB_HOST'),
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> getenv('DB_USER'),
-    'password'	=> getenv('DB_PASS'),
-    'database'	=> getenv('DB_NAME'),
+    'username'	=> 'sql12801932',
+    'password'	=> 'rjcd114sJA',
+    'database'	=> 'sql12801932',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
